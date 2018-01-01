49029916cba7e1ac52091ed7508c5f742f4fbe2f
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2018-01-01 22:20:01 +0100

    Tests: Add failing tests

---
+++ Test/Nett.Tests/FailingTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using Xunit;
+
+namespace Nett.Tests
+{
+    [ExcludeFromCodeCoverage]
+    public class FailingTests
+    {
+        private class Foo
+        {
+            public DateTime Timestamp { get; set; }
+        }
+
+        [Fact]
+        public void WriteAndReadDateTime()
+        {
+            // Arrange
+            var subject = new Foo { Timestamp = DateTime.Now };
+            var expected = Toml.WriteString(subject);
+
+            // Act
+            var result = Toml.WriteString(Toml.ReadString<Foo>(expected));
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        private class Parent
+        {
+            public Child Child { get; set; }
+        }
+
+        private class Child
+        {
+            public Parent Parent { get; set; }
+        }
+
+        [Fact(Skip = "Stack overflow exception occurred in test. Test run is aborted.")]
+        public void WriteCircularReference()
+        {
+            // Arrange
+            var subject = new Parent();
+            subject.Child = new Child { Parent = subject };
+
+            // Act
+            Toml.WriteString(subject);
+        }
+    }
+}
